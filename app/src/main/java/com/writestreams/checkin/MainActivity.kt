c328f99a110bd16f1b3e85321464062ea005593f
Derek Williams <derekwlms@gmail.com>
2025-01-01 14:21:22 -0600

    Get cached persons on startup

--- app/src/main/java/com/writestreams/checkin/MainActivity.kt
+++ app/src/main/java/com/writestreams/checkin/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.writestreams.checkin
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.writestreams.checkin.data.repository.Repository
 import com.writestreams.checkin.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var repository: Repository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +30,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_checkin, R.id.navigation_attendance, R.id.navigation_settings
@@ -31,5 +37,25 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        repository = Repository(applicationContext)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val cachedPersons = repository.getCachedPersons()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity,
+                        "Found ${cachedPersons.size} persons", Toast.LENGTH_SHORT).show()
+                    Log.i("MainActivity", "Found ${cachedPersons.size} cached persons")
+                }
+                if (cachedPersons.size < 10) {
+                    repository.fetchAndCachePersons()
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Log.e("MainActivity", "Error fetching and caching persons", e)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/writestreams/checkin/data/repository/Repository.kt
+++ app/src/main/java/com/writestreams/checkin/data/repository/Repository.kt
@@ -6,11 +6,16 @@ import com.writestreams.checkin.data.local.AppDatabase
 import com.writestreams.checkin.data.local.Person
 import com.writestreams.checkin.data.local.PersonDao
 import com.writestreams.checkin.data.network.BreezeChmsApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class Repository(context: Context) {
     private val apiService: BreezeChmsApiService
@@ -32,23 +37,37 @@ class Repository(context: Context) {
         personDao = db.personDao()
     }
 
-    fun fetchAndCachePersons() {
-        apiService.getPersons().enqueue(object : Callback<List<Person>> {
-            override fun onResponse(call: Call<List<Person>>, response: Response<List<Person>>) {
-                if (response.isSuccessful) {
-                    response.body()?.let { persons ->
-                        personDao.insertAll(persons)
+    suspend fun fetchAndCachePersons() {
+        val persons = withContext(Dispatchers.IO) {
+            suspendCancellableCoroutine<List<Person>> { continuation ->
+                apiService.getPersons().enqueue(object : Callback<List<Person>> {
+                    override fun onResponse(
+                        call: Call<List<Person>>,
+                        response: Response<List<Person>>
+                    ) {
+                        if (response.isSuccessful) {
+                            response.body()?.let { continuation.resume(it) }
+                                ?: continuation.resumeWithException(NullPointerException("Response body is null"))
+                        } else {
+                            continuation.resumeWithException(Exception("Response not successful"))
+                        }
                     }
-                }
-            }
 
-            override fun onFailure(call: Call<List<Person>>, t: Throwable) {
-                // Handle failure
+                    override fun onFailure(call: Call<List<Person>>, t: Throwable) {
+                        continuation.resumeWithException(t)
+                    }
+                })
             }
-        })
+        }
+
+        withContext(Dispatchers.IO) {
+            personDao.insertAll(persons)
+        }
     }
 
-    fun getCachedPersons(): List<Person> {
-        return personDao.getAllPersons()
+    suspend fun getCachedPersons(): List<Person> {
+        return withContext(Dispatchers.IO) {
+            personDao.getAllPersons()
+        }
     }
 }
