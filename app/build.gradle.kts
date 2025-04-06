b24505a129941d48fdd1d4c6b8fda190d758138a
Derek Williams <derekwlms@gmail.com>
2025-04-06 09:39:19 -0400

    Tag 1.3.1 with add guest updates, but not full full offline sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.4"
+        versionName = "1.3.1"
         buildConfigField("String", "BUILD_DATE", "\"${SimpleDateFormat("yyyy-MM-dd").format(Date())}\"")
 
 
